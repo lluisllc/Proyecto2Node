48e101fafe3a9be7590d635b798faf2e7db229aa
lluisllc <lluis.llacera@gmail.com>
2022-07-07 10:55:50 +0200

    Version estable v8

--- utils/consts.js
+++ utils/consts.js
@@ -1,3 +1,3 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/Proyecto2Node";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/Proyecto2Node";
 
 module.exports = MONGO_URI;
